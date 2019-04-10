50c4f2e7030a9f2c07887fba69a69c9de9149125
scjqwe <2290676174@qq.com>
2019-04-11 00:32:17 +0800

    项目创建

---
+++ README.md
@@ -0,0 +1,4 @@
+# spring-boot-alibaba-sentinel-examples
+Alibaba Sentinel简单使用
+
+[官方文档](https://github.com/alibaba/Sentinel/wiki/%E4%BB%8B%E7%BB%8D)
---
+++ src/main/java/com/sentinel/SpringBootAlibabaSentinelExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAlibabaSentinelExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAlibabaSentinelExamplesApplication.class, args);
+	}
+
+}
